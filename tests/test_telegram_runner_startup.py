20c26184ee495e3a063e8be27b5866ac44a6259a
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-20 23:05:55 +0700

    Remove accidental test file from main

--- tests/test_telegram_runner_startup.py
+++
@@ -1,8 +0,0 @@
-from telegram_bot_runner import _personal_welcome
-
-
-def test_personal_welcome_uses_first_name():
-    text = _personal_welcome("Дмитрий")
-    assert "Привет, Дмитрий!" in text
-    assert "Мама, тут дешевле!" in text
-    assert "Могу сама искать" in text
